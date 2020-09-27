be1382a6a81d5feae8f58ede813b035b0eb9631c
claudinemahoro <claudinemah3@gmail.com>
2020-09-27 21:29:25 +0200

    modify the css file

---
+++ js1/index.js
@@ -0,0 +1,5 @@
+function getAkan() {
+  var input = document.getElementById("birth").value;
+  var myDate = new Date(input);
+  var gender= document.querySelector('input[name="Gender"]:checked').value;
+}
