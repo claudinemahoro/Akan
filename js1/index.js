743e296204625a13372f5a848b3b32e3a6923cc4
claudinemahoro <claudinemah3@gmail.com>
2020-09-27 21:32:12 +0200

    get the values entered through the form in js file and determine the year,month and date in the entered values

--- js1/index.js
+++ js1/index.js
@@ -2,4 +2,8 @@ function getAkan() {
   var input = document.getElementById("birth").value;
   var myDate = new Date(input);
   var gender= document.querySelector('input[name="Gender"]:checked').value;
+  yyyy = myDate.getFullYear();
+  mm = myDate.getMonth();
+  mm = mm+1;
+  dd = myDate.getDate();
 }
