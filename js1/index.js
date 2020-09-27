3e7ee4169a671bd1c4e241b1b8e9df2e11c06633
claudinemahoro <claudinemah3@gmail.com>
2020-09-27 21:34:06 +0200

    determine the century in the entered year and calculate the day of the week

--- js1/index.js
+++ js1/index.js
@@ -6,4 +6,8 @@ function getAkan() {
   mm = myDate.getMonth();
   mm = mm+1;
   dd = myDate.getDate();
+  var c =(yyyy-1)/100+1;
+  var cc = parseFloat(c);
+  var day = parseFloat(( ((cc/4) -2*cc-1) + ((5*yyyy/4)) + ((26*(mm+1)/10)) + dd )) % 7;
+  day = day.toFixed(0);
 }
